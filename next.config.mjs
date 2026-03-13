96855103df49a1d7b12ecf4205918f4410a07f77
Waqar Niyazi <niyazi.waqar007@gmail.com>
2026-03-13 19:02:47 +0530

    mdx loader fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 import createMDX from '@next/mdx'
-import remarkFrontmatter from 'remark-frontmatter'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -16,8 +14,8 @@ const nextConfig = {
 const withMDX = createMDX({
   options: {
     remarkPlugins: [
-      remarkFrontmatter,
-      remarkMdxFrontmatter,
+      ['remark-frontmatter'],
+      ['remark-mdx-frontmatter'],
     ],
   },
 })
